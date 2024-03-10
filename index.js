95437a27989346e0f4e5bf74b7bac546a40fa48b
lenorgest0 <162015334+lenorgest0@users.noreply.github.com>
2024-03-10 09:18:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// 导入所需的模块
+const projectInstaller = require('project-installer');
+const projectFunctionality = require('project-functionality');
+
+// 安装项目
+projectInstaller.installProject();
+
+// 启动功能
+projectFunctionality.start();
